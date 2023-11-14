3c51a15a6911743a6250e99bfab5e293ff036a38
adrianmartins-cps <adrian.silva64@etec.sp.gov.br>
2023-11-14 20:49:08 -0300

    aula1411 fundamentos dom html

--- public/js/action.js
+++ public/js/action.js
@@ -1,13 +1,26 @@
-function somaNumero(n1, n2, op){
- let resultado;
- if(op==="c"){
-    resultado = (n1 + n2)
-    console.table(resultado)
-    return
- }
- if(op==="a"){
-    resultado = (n1 - n2)
-    alert(resultado)
-    return
- }
+function calculaNumeros(){
+let n1 = parseInt(document.getElementById("usuario").value)
+let n2 = parseInt(document.getElementById("senha").value)
+let op = document.getElementById("operation").value
+let resultado
+if (op ==="+") {
+   resultado = (n1 + n2)
+
+alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
+}else if (op === "-") {
+   resultado = (n1 - n2)
+   alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
+}else if (op === "*") {
+      resultado = (n1 * n2)
+      alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
+   }else if (op === "/") {
+      resultado = (n1 / n2)
+      alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
+   }else if(op ==="sqrt"){
+   resultado= Math.sqrt(n1+n2)
+   alert("RAIZ: "+resultado)
+   }else if(op ==="sqrt"){
+      resultado= Math.power(n1+n2)
+      alert("Potencia: "+resultado)
+}
 }
