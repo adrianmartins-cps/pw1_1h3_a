734c32262c7b3d535d4db442c4604400204d9c17
adrianmartins-cps <adrian.silva64@etec.sp.gov.br>
2023-11-21 20:42:49 -0300

    aula2111 Modulos em javaescript

--- public/js/action.js
+++ public/js/action.js
@@ -1,26 +1,10 @@
-function calculaNumeros(){
-let n1 = parseInt(document.getElementById("usuario").value)
-let n2 = parseInt(document.getElementById("senha").value)
-let op = document.getElementById("operation").value
-let resultado
-if (op ==="+") {
-   resultado = (n1 + n2)
+import userlogin from './moduloDados.js'
 
-alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
-}else if (op === "-") {
-   resultado = (n1 - n2)
-   alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
-}else if (op === "*") {
-      resultado = (n1 * n2)
-      alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
-   }else if (op === "/") {
-      resultado = (n1 / n2)
-      alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
-   }else if(op ==="sqrt"){
-   resultado= Math.sqrt(n1+n2)
-   alert("RAIZ: "+resultado)
-   }else if(op ==="sqrt"){
-      resultado= Math.power(n1+n2)
-      alert("Potencia: "+resultado)
-}
-}
+const login = document.getElementById("login")
+
+login.addEventListener("click", (e) => {
+   let user = document.getElementById("usuario").value
+   let senha = document.getElementById("senha").value
+   console.table(user, senha)
+   console.table(userlogin)
+})
