aa2bfad6901f675f890fbb692d3fc5ff66393cf4
adrianmartins-cps <adrian.silva64@etec.sp.gov.br>
2023-11-22 22:38:41 -0300

    aula2211 arreys e interações

--- public/js/action.js
+++ public/js/action.js
@@ -1,10 +1,16 @@
 import userlogin from './moduloDados.js'
 
 const login = document.getElementById("login")
+const cadastro = document.getElementById("cadastro")
 
 login.addEventListener("click", (e) => {
    let user = document.getElementById("usuario").value
    let senha = document.getElementById("senha").value
-   console.table(user, senha)
-   console.table(userlogin)
 })
+cadastro.addEventListener("click", (e) => {
+   userlogin.find((userlogin) => userLogin.email === "victorsena301@gmail.com")
+   console.log(usuario)
+})
+function encontraUsuario(value, index, array){
+console.table(value.email)
+}
